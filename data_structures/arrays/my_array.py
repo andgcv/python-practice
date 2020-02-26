1e5f80df584173881d7788e23a1ea66bd1e2b0fd
André Gonçalves <andgcv@gmail.com>
2020-02-26 21:18:34 +0000

    Array implementation -> .pop(), .delete(), .remove(), rest of tests

--- data_structures/arrays/my_array.py
+++ data_structures/arrays/my_array.py
@@ -26,54 +26,91 @@ class Array:
 
     # Return the item at a given index, if the index is within bounds
     def at(self, index):
-        if index < 0 or index >= self._size:         # Check if index is out of bounds
+        if index < 0 or index >= self._size:      # Check if index is out of bounds
             raise IndexError('Must enter a valid index')
         return self._array[index]
 
     # Return the index of the item we're searching for within our array, return -1 if not found
     def find(self, item):
         for i in range(self._size):
-            if self._array[i] is item:          # Check if current value of array is strictly equal to the given item
-                return i                        # Found the item, return its index
-        return -1                               # Didn't find the item, return -1
+            if self._array[i] is item:            # Check if current value of array is strictly equal to the given item
+                return i                          # Found the item, return its index
+        return -1                                 # Didn't find the item, return -1
 
     # Push an item to the end of the array
     def push(self, item):
-        if self._size == self._capacity:                # Check if array size is at max capacity
+        if self._size == self._capacity:          # Check if array size is at max capacity, adjust accordingly
             self._resize(self._capacity * 2)
         self._array[self._size] = item
         self._size += 1
 
     # Insert an item in the desired index of the array
     def insert(self, index, item):
-        if index < 0 or index > self._size:      # Check if index is out of bounds
+        if index < 0 or index > self._size:       # Check if index is out of bounds
             raise IndexError('Must enter a valid index (for contiguous memory)')
-        if self._size == self._capacity:         # Check if array size is at max capacity
+        if self._size == self._capacity:          # Check if array size is at max capacity, adjust accordingly
             self._resize(self._capacity * 2)
-        if not index == self._size:              # Check if the index I chose is the tail of my current array
-            self._shift_indexes(index, True)     # If it is not the tail, shift the indexes accordingly
+        if not index == self._size:               # Check if the index I chose is the tail of my current array
+            self._shift_indexes(index, True)      # If it is not the tail, shift the indexes accordingly
         else:
-            self._size += 1                      # Update size manually, because ._shift_indexes wasn't called
+            self._size += 1                       # Update size manually, because ._shift_indexes wasn't called
         self._array[index] = item
 
     # Prepend an item at the head of the array
     def prepend(self, item):
-        if self._size == self._capacity:         # Check if size is at max capacity
+        if self._size == self._capacity:          # Check if array size is at max capacity, adjust accordingly
             self._resize(self._capacity * 2)
-        if not self.is_empty():                  # Check if array is empty, aka size is 0
-            self._shift_indexes(0, True)         # If it is not empty, shift the indexes accordingly
+        if not self.is_empty():                   # Check if array is empty, aka size is 0
+            self._shift_indexes(0, True)          # If it is not empty, shift the indexes accordingly
         else:
-            self._size += 1                      # Update size manually, because ._shift_indexes wasn't called
+            self._size += 1                       # Update size manually, because ._shift_indexes wasn't called
         self._array[0] = item
+
+    # Pop the tail of the array and return it
+    def pop(self):
+        _temp_tail = self._array[self._size - 1]  # Store the tail of the array in a variable so we can return it later
+        self._array[self._size - 1] = None        # Remove the tail of the array
+        self._size -= 1
+        if self._size <= self._capacity / 4:      # Check if array size is 1/4 of max capacity, adjust accordingly
+            self._resize(int(self._capacity / 2))
+        return _temp_tail
+
+    # Delete the item at the given index of the array and return it
+    def delete(self, index):
+        if index < 0 or index >= self._size:      # Check if index is out of bounds
+            raise IndexError('Must enter a valid index')
+        _temp_deleted = self._array[index]        # Store the deleted item of the array in a variable to return later
+        self._array[index] = None                 # Delete the item at the given index
+        if not index == self._size - 1:           # If the index is not the tail, will have to shift indexes
+            self._shift_indexes(index, False)
+        else:
+            self._size -= 1                       # Update size manually, because ._shift_indexes wasn't called
+        if self._size <= self._capacity / 4:      # Check if array size is 1/4 of max capacity, adjust accordingly
+            self._resize(int(self._capacity / 2))
+        return _temp_deleted
+
+    # Remove the item with the value passed in, and return its index
+    def remove(self, item):
+        _found_index = self.find(item)            # Store the index returned from .find() in a variable
+        if _found_index is -1:                    # Check if the returned index is -1, if so, raise a ValueError
+            raise ValueError('Item not found, please enter a valid value')
+        self._array[_found_index] = None          # Remove the item from the array
+        if not _found_index == self._size - 1:    # If the index found is not the tail, will have to shift indexes
+            self._shift_indexes(_found_index, False)
+        else:
+            self._size -= 1                       # Update size manually, because ._shift_indexes wasn't called
+        if self._size <= self._capacity / 4:      # Check if array size is 1/4 of max capacity, adjust accordingly
+            self._resize(int(self._capacity / 2))
+        return _found_index
 
     # (Private) Resize the array with the given (new) capacity
     def _resize(self, new_capacity):
-        _temp_array = [None] * new_capacity     # Allocate desired memory to a temporary array
+        _temp_array = [None] * new_capacity       # Allocate desired memory to a temporary array
         for i in range(self._size):
-            _temp_array[i] = self._array[i]     # Copy each value from the original array to the temporary array
-        self._array = _temp_array               # Update the original array to its resized version
-        _temp_array = None                      # Deallocate memory from temporary array
-        self._capacity = new_capacity           # Update the capacity so it matches the resized array
+            _temp_array[i] = self._array[i]       # Copy each value from the original array to the temporary array
+        self._array = _temp_array                 # Update the original array to its resized version
+        _temp_array = None                        # Deallocate memory from temporary array
+        self._capacity = new_capacity             # Update the capacity so it matches the resized array
 
     # (Private) Shift indexes to the right, or to the left, and update the array's size
     def _shift_indexes(self, start_index, shift_right):
--- data_structures/arrays/test_array.py
+++ data_structures/arrays/test_array.py
@@ -4,27 +4,30 @@ if __name__ == '__main__':
     capacity = 4
     array = Array(capacity)
 
+    print('-- Beginning of testing --')
+    print('')
+
     # Tests with initial values and capacity of 4
-    print('Array size: {}'.format(array.get_size()))                             # Should return 0
-    print('Array capacity: {}'.format(array.get_capacity()))                     # Should return 4
-    print('Array is empty: {}'.format(array.is_empty()))                         # Should return True
+    print('Array size: {}'.format(array.get_size()))                                        # Should return 0
+    print('Array capacity: {}'.format(array.get_capacity()))                                # Should return 4
+    print('Array is empty: {}'.format(array.is_empty()))                                    # Should return True
     print('')
 
     # Push items into the array
     array.push(7)
     array.push(30)
-    print('Array value at index 0: {}'.format(array.at(0)))                      # Should return 7
-    print('Find index of item 30: {}'.format(array.find(30)))                    # Should return 1
+    print('Array value at index 0: {}'.format(array.at(0)))                                 # Should return 7
+    print('Find index of item 30: {}'.format(array.find(30)))                               # Should return 1
     print('')
 
     # Reach max capacity and resize the array
     array.push(3)
     array.push(10)
     array.push(2)
-    print('Array size after pushing values: {}'.format(array.get_size()))        # Should return 5
-    print('Capacity after resizing Array: {}'.format(array.get_capacity()))      # Should return 8
-    print('Array is empty, after pushing values: {}'.format(array.is_empty()))   # Should return False
-    print('Array value at index 4, after resizing: {}'.format(array.at(4)))      # Should return 2
+    print('Array size after pushing values: {}'.format(array.get_size()))                   # Should return 5
+    print('Capacity after resizing Array: {}'.format(array.get_capacity()))                 # Should return 8
+    print('Array is empty, after pushing values: {}'.format(array.is_empty()))              # Should return False
+    print('Array value at index 4, after resizing: {}'.format(array.at(4)))                 # Should return 2
     print('')
 
     # Insert items into the head, the middle, and the tail of the array
@@ -46,3 +49,32 @@ if __name__ == '__main__':
     print('Array value at index 0, after prepending value 42: {}'.format(array.at(0)))      # Should return 42
     print('Array size after prepending all values: {}'.format(array.get_size()))            # Should return 10
     print('Capacity after prepending all values: {}'.format(array.get_capacity()))          # Should return 16
+    print('')
+
+    # Pop items from the tail of the array
+    print('Value of the item popped from the tail: {}'.format(array.pop()))                 # Should return 9
+    print('Value of the item popped from the tail: {}'.format(array.pop()))                 # Should return 2
+    print('Value of the item popped from the tail: {}'.format(array.pop()))                 # Should return 10
+    print('')
+
+    # Delete items from the array at the given indexes
+    print('Value of the item deleted at first index: {}'.format(array.delete(0)))           # Should return 42
+    print('Value of the item deleted at index 3: {}'.format(array.delete(3)))               # Should return 30
+    print('Value of the item deleted at last index: {}'.format(array.delete(array.get_size()-1)))  # Should return 3
+    print('')
+
+    # Remote items from the array, with the given values
+    print('Index of the item 15 removed: {}'.format(array.remove(15)))                      # Should return 1
+    print('Index of the item 4 removed: {}'.format(array.remove(4)))                        # Should return 2
+    print('Index of the item 7 removed: {}'.format(array.remove(7)))                        # Should return 1
+    print('Index of the item 1 removed: {}'.format(array.remove(1)))                        # Should return 0
+    # print('Index of the item 30 removed: {}'.format(array.remove(30)))                    # Should raise ValueError
+    print('')
+
+    # Size, capacity and if empty or not, after all tests
+    print('Array size, after all the tests: {}'.format(array.get_size()))                   # Should return 0
+    print('Array capacity, after all the tests: {}'.format(array.get_capacity()))           # Should return 1
+    print('Array is empty, after all the tests: {}'.format(array.is_empty()))               # Should return True
+    print('')
+
+    print('-- End of testing --')
